5ab87b16252885e9c23da9c3772b93b1f2fe3c24
Bartek <bartek9393@poczta.onet.pl>
2017-02-09 13:38:19 +0100

    Added frame with button to click

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,34 @@
+import javax.swing.*;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
 public class Application {
-    
+
+    private void displayBoard() {
+        JFrame frame = new JFrame("Clicker");
+
+        JButton button = new JButton();
+        button.setText("Click me!");
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == 1) {
+                    System.out.println("Du");
+                }
+            }
+        });
+        frame.add(button);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Application().displayBoard());
+    }
 }
